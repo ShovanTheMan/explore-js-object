dd2c31ce9d7d688dc69b9d6b3da5b8113b6ca235
ShovanTheMan <shovan2192@gmail.com>
2024-02-07 19:25:45 +0600

    arrow added

--- this.js
+++ this.js
@@ -1,1 +1,32 @@
-console.log(this);
+// console.log(this);
+const kodomAli = {
+    name: 'kodom ali',
+    money: 5000,
+    study: 'Math',
+    subjects: ['calculas', 'algebra','geometry'],
+    exam: function(){
+        return this.name + 'is participate in an exam';
+    },
+    examArrow: () => {
+        console.log(this);
+    };
+    examNested: () => {
+        const arrow = () => {
+            console.log(this);
+        }
+    }
+    improveExam: function(subject){
+        this.exam();
+        return `${this.name} is taking improvement exam on ${subject}`
+    },
+    treatDey: function(amount, tips){
+        this.money = this.money - amount - tips;
+        return this.money;
+
+    }
+}
+kodomAli.exam();
+
+
+badamAli.exam = kodomAli.exam;
+badamAli.exam();
